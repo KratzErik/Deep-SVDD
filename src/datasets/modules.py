2503a04a5cf49cea08c3ad1acee1435f7beec6e6
ErikKratzCth <erik.kj.kratz@gmail.com>
2018-11-08 08:35:04 +0100

    Update modules.py

--- src/datasets/modules.py
+++ src/datasets/modules.py
@@ -3,7 +3,7 @@ from lasagne.init import GlorotUniform, Constant
 
 
 def addConvModule(nnet, num_filters, filter_size, pad='same', W_init=None, bias=True, use_maxpool = True, pool_size=(2,2),
-                  use_batch_norm=False, dropout=False, p_dropout=0.5, upscale=False):
+                  use_batch_norm=False, dropout=False, p_dropout=0.5, upscale=False, dilation = (1,1), stride = (1,1)):
     """
     add a convolutional module (convolutional layer + (leaky) ReLU + MaxPool) to the network  
     """
