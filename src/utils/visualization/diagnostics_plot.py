9960934e75ac192b17b79499460f17f9c73630d6
ErikKratzCth <erik.kj.kratz@gmail.com>
2018-11-12 12:09:16 +0000

    Deleted logs

--- src/utils/visualization/diagnostics_plot.py
+++ src/utils/visualization/diagnostics_plot.py
@@ -264,6 +264,7 @@ def plot_random_reconstructions(nnet, xp_path, title_suffix, file_prefix, n_img=
 
     # only plot reconstructions for image data
     if nnet.data._X_train.ndim != 4:
+        print("Wrong input shape: not exporting reconstruction samples")
         return
 
     from utils.visualization.mosaic_plot import plot_mosaic
@@ -275,7 +276,9 @@ def plot_random_reconstructions(nnet, xp_path, title_suffix, file_prefix, n_img=
 
     title = str(n_img) + " random autoencoder reconstructions " + title_suffix
     plot_mosaic(reconstruction, title=title, export_pdf=(xp_path + "/" + file_prefix + "ae_reconstructions"))
+    print("Exported random reconstructions")
 
     # Also plot the corresponding input, for comparison
     title = str(n_img) + " random autoencoder inputs " + title_suffix
     plot_mosaic(nnet.data._X_train[random_idx, ...], title=title, export_pdf=(xp_path + "/" + file_prefix + "ae_inputs"))
+    print("Exported random inputs")
